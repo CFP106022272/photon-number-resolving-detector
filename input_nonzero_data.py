9ac9554a5da0ae39800c930d2e0c714e2542ac77
Saver <48315174+CFP106022272@users.noreply.github.com>
2021-01-24 11:52:54 +0800

    Update input_nonzero_data.py

--- input_nonzero_data.py
+++ input_nonzero_data.py
@@ -88,4 +88,39 @@ for i in range(len(data_list)):
     columnH = sweeps_list[i]
     sheet.append([columnA,columnB,columnC,columnD,columnE,columnF,columnG,columnH])
 
-excel_file.save('nonzero_data.xlsx')
+excel_file.save('nonzero_data(test).xlsx')
+
+#Statistics
+count1_list=[]
+count2_list=[]
+count3_list=[]
+count4_list=[]
+count5_list=[]
+count6_list=[]
+count_else_list=[]
+
+for i in range(len(sweeps_list)):
+    if sweeps_list[i] != sweeps_list[i-1]:
+        count1_list.append(1)
+    elif sweeps_list[i] == sweeps_list[i-1] and sweeps_list[i] != sweeps_list[i-2]:
+        count2_list.append(2)
+    elif sweeps_list[i] == sweeps_list[i-1] and sweeps_list[i] == sweeps_list[i-2] and sweeps_list[i] != sweeps_list[i-3]:
+        count3_list.append(3)
+    elif sweeps_list[i] == sweeps_list[i-1] and sweeps_list[i] == sweeps_list[i-2] and sweeps_list[i] == sweeps_list[i-3] and sweeps_list[i] != sweeps_list[i-4]:
+        count4_list.append(4)
+    elif sweeps_list[i] == sweeps_list[i-1] and sweeps_list[i] == sweeps_list[i-2] and sweeps_list[i] == sweeps_list[i-3] and sweeps_list[i] == sweeps_list[i-4] and sweeps_list[i] != sweeps_list[i-5]:
+        count5_list.append(5)
+    elif sweeps_list[i] == sweeps_list[i-1] and sweeps_list[i] == sweeps_list[i-2] and sweeps_list[i] == sweeps_list[i-3] and sweeps_list[i] == sweeps_list[i-4] and sweeps_list[i] == sweeps_list[i-5] and sweeps_list[i] != sweeps_list[i-6]:
+        count6_list.append(6)
+    else:
+        count_else_list.append(7)
+
+count_else = len(count_else_list)
+count6 = len(count6_list) - count_else
+count5 = len(count5_list) - count6 - count_else
+count4 = len(count4_list) - count5 - count6 - count_else
+count3 = len(count3_list) - count4 - count5 - count6 - count_else
+count2 = len(count2_list) - count3 - count4 - count5 - count6 - count_else
+count1 = len(count1_list) - count2 - count3 - count4 - count5 - count6 - count_else
+
+print("count1:",count1,"\n","count2:",count2,"\n","count3:",count3,"\n","count4:",count4,"\n","count5:",count5,"\n","count6:",count6,"\n","count_else:",count_else)
