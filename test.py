7d5d778cbaf7d34506e4e49495443f246ef5f024
Saver <48315174+CFP106022272@users.noreply.github.com>
2021-01-25 21:26:57 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,3 @@
-data_list=[]
-
-with open("single_photon.txt", mode="r") as file:
-    for line in file:
-        if str(line[4:11]) == str("0000000"):
-            0
-        else:
-            data_list.append(line)
-
-#print(data_list)
-with open("nonzero_data_1.txt", mode="w") as file:
-    file.write(str(data_list))
+list=[0,1,9,9,4,2,9]
+find = 9
+print([i for i,v in enumerate(list) if v==find])
